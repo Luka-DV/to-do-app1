5009871f3b565d883c881b0a26edbd6d2f8639bd
Luka-DV <luka.guva@gmail.com>
2024-08-28 09:05:27 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,90 @@
+
+const express = require("express");
+const app = express();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const PORT = process.env.PORT;
+const connectionUri = process.env.connectionString;
+
+const client = new MongoClient(connectionUri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+async function runClient() {
+
+    try {
+        await client.connect(); //connect to DB(optional)
+        console.log('Connected to Le Database!');
+    
+        const db = client.db("to-do-app"); //database
+        const taskCollection = db.collection("tasks") //returns reference/creates a new db colleciton
+
+            
+        app.get('/',(request, response)=>{
+            db.collection('rappers').find().sort({likes: -1}).toArray()
+            .then(data => {
+                response.render('index.ejs', { info: data })
+            })
+            .catch(error => console.error(error))
+        })
+
+        app.post('/addRapper', (request, response) => {
+            db.collection('rappers').insertOne({stageName: request.body.stageName,
+            birthName: request.body.birthName, likes: 0})
+            .then(result => {
+                console.log('Rapper Added')
+                response.redirect('/')
+            })
+            .catch(error => console.error(error))
+        })
+
+        app.put('/addOneLike', (request, response) => {
+            db.collection('rappers').updateOne({stageName: request.body.stageNameS, birthName: request.body.birthNameS,likes: request.body.likesS},{
+                $set: {
+                    likes:request.body.likesS + 1
+                }
+            },{
+                sort: {_id: -1},
+                upsert: true
+            })
+            .then(result => {
+                console.log('Added One Like')
+                response.json('Like Added')
+            })
+            .catch(error => console.error(error))
+
+        })
+
+        app.delete('/deleteRapper', (request, response) => {
+            db.collection('rappers').deleteOne({stageName: request.body.stageNameS})
+            .then(result => {
+                console.log('Rapper Deleted')
+                response.json('Rapper Deleted')
+            })
+            .catch(error => console.error(error))
+
+        })
+        
+        } catch (error) {
+            console.error(error)
+        }
+
+}
+
+runClient();
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT# ${PORT}. You better go catch it!`);
+})
